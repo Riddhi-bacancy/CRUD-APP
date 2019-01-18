7511be2b495a3367dc39df28e06d4664cd9ec8b7
Riddhi-bacancy <riddhi.pujara@bacancytechnology.com>
2019-01-18 17:19:33 +0530

    only create

--- Routers/api.js
+++ Routers/api.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Ninja = require('../models/ninja');
-router.get('/ninjas',function(req,res,next){
-	Ninja.find({}).then(function(ninja){
-		res.send(ninja);
-	})
-});
 
 router.post('/ninjas',function(req,res,next){
 	  
@@ -16,19 +11,4 @@ router.post('/ninjas',function(req,res,next){
 		
 });
 
-router.put('/ninjas/:id',function(req,res,next){
-	Ninja.findByIdAndUpdate({_id : req.params.id},req.body).then(function(ninja){
-		Ninja.findOne({_id : req.params.id}).then(function(ninja){
-
-           res.send(ninja);
-    	});
-    });
-});
-
-router.delete('/ninjas/:id',function(req,res,next){
-    Ninja.findByIdAndRemove({_id : req.params.id}).then(function(ninja){
-       res.send(ninja);
-    })
-	
-});
 module.exports = router;
