e6af05a7000e7c797c2d3ccef6ce5399b0865d0a
Riddhi-bacancy <riddhi.pujara@bacancytechnology.com>
2019-01-18 16:58:14 +0530

    only create

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 const mongoose = require('mongoose');
-
-
+var path = __dirname + "/views/";
 const app = express();
-mongoose.connect('mongodb://localhost/ninjago');
+app.use("/views",express.static(__dirname + "/views"));
+app.use("/js",express.static(__dirname + "/public/"));
+
+mongoose.connect('mongodb://localhost/API');
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
+
+app.get("/",function(req,res){
+	res.sendFile(path + "index1.html");
+});
 app.use('/api',require('./Routers/api'));
-app.use(function(err,req,res,next){
+/*app.use(function(err,req,res,next){
   //console.log(err);
   res.send({error:err.message})
-})
+})*/
 app.listen(8585);
