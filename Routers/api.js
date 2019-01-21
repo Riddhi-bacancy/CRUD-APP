c2100104ea072fff6ab3ffd9e52c28e531d1b3f4
Riddhi-bacancy <riddhi.pujara@bacancytechnology.com>
2019-01-21 12:42:18 +0530

    complted GUI for CRUD

--- Routers/api.js
+++ Routers/api.js
@@ -10,5 +10,31 @@ router.post('/ninjas',function(req,res,next){
 	
 		
 });
+router.put('/ninjas/Update',function(req,res,next){
+	temp = {firstname : req.body.firstname };
+	console.log(temp);
+	console.log(req.body);
+	   Ninja.findOneAndUpdate(temp,{$set : {firstname : req.body.firstname,lastname : req.body.lastname}}).then(function(ninja){
+	   	res.send(ninja);
+	   }).catch(next);
+});
+let temp;
+router.delete('/ninjas/delete',function(req,res,next){
+	console.log(req.body);
+	   Ninja.remove(req.body).then(function(ninja){
+	   	res.send(ninja);
+	   }).catch(next);
+})
+
+router.get('/ninjas/Read',function(req,res,next){
+	console.log(req.query.firstname);
+	temp = {firstname : req.query.firstname,
+	       lastname : req.query.lastname
+	        };
+	        console.log(temp);
+	   Ninja.find(temp).then(function(ninja){
+	   	res.send(ninja);
+	   }).catch(next);
+})
 
 module.exports = router;
