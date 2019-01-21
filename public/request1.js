bd77ca9bdd041534de1f7ce152ecb246a85eb566
Riddhi-bacancy <riddhi.pujara@bacancytechnology.com>
2019-01-21 12:42:02 +0530

    complted GUI for CRUD

--- public/request1.js
+++ public/request1.js
@@ -1,15 +1,96 @@
 $( document ).ready(function() {
-	alert("sucesss");
+
 	// SUBMIT FORM
-    $("#customerForm").submit(function(event) {
+    $("#ins").click(function(event) {
 		// Prevent the form from submitting via the browser.
 		event.preventDefault();
-		ajaxPost();
+		InsertData();
 
 	});
+	$("#del").click(function(event) {
+		event.preventDefault();
+		DeleteData();
+	})
+
+	$("#read").click(function(event) {
+		event.preventDefault();
+		ReadData();
+	})
+
+	$("#udt").click(function(event) {
+		event.preventDefault();
+		UpdateData();
+	})
+    function UpdateData(){
+       var formData = {
+       	 firstname : $("#firstname").val(),
+    	 lastname :  $("#lastname").val()
+       }
+       $.ajax({
+       	type : "PUT",
+       	contentType : "application/json",
+       	url :  "http://localhost:8585/api/ninjas/Update",
+		data : JSON.stringify(formData),
+		dataType : 'json',
+		success : function(res) {
+				console.log(res);
+			},
+			error : function(e) {
+		
+				alert("Error!")
+				console.log("ERROR: ", e);
+			}
+       });
+       resetData();
+    };
+	function ReadData(){
+       var formData = {
+       	 firstname : $("#firstname").val(),
+    	 lastname :  $("#lastname").val()
+       }
+       $.ajax({
+       	type : "GET",
+       	contentType : "application/json",
+       	url :  "http://localhost:8585/api/ninjas/Read",
+		data : formData,
+		dataType : 'json',
+		success : function(res) {
+				console.log(res);
+				$("#display").html("<p>" + 
+				"GET Successfully! <br>" +
+					"--->" + JSON.stringify(res)+ "</p>"); 
+				
+			},
+			error : function(e) {
+				alert("Error!")
+				console.log("ERROR: ", e);
+			}
+       });
+       resetData();
+    };
     
-    
-   function ajaxPost(){
+    function DeleteData(){
+       var formData = {
+       	 firstname : $("#firstname").val(),
+    	 lastname :  $("#lastname").val()
+       }
+       $.ajax({
+       	type : "DELETE",
+       	contentType : "application/json",
+       	url :  "http://localhost:8585/api/ninjas/delete",
+		data : JSON.stringify(formData),
+		dataType : 'json',
+		success : function(res) {
+				console.log(res);
+			},
+			error : function(e) {
+				alert("Error!")
+				console.log("ERROR: ", e);
+			}
+       });
+       resetData();
+    };
+   function InsertData(){
     	
     	// PREPARE FORM DATA
     	var formData = {
